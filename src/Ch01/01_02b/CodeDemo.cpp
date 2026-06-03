1dd65fa9f751de44a22e56a5643536067a5d9c2d
Kanishka Kamal <145517861+kknis@users.noreply.github.com>
2026-06-03 19:52:31 +0000

    Finished chapters

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,13 @@
 // Complete Guide to C++ Programming Foundations
-// Exercise 01_02
+// Exercise 01_02 Modified!
 // Hello World, by Eduardo Corpeño 
+
+# include <iostream>
+
+int main(){
+    std::cout << "Hello World!" << std::endl;
+
+    std::cout << std::endl;
+    std::cout << std::endl; // two for my preferences
+
+}
